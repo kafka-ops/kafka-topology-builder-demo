a25152170cf10df5c25d6813072c61e0bafddf90
Pere Urbon <pere@confluent.io>
2020-05-02 20:14:23 +0200

    add post hooks for update status

---
+++ post-hook-failure.sh
@@ -0,0 +1,4 @@
+curl "https://api.github.com/repos/purbon/kafka-topology-builder-demo/statuses/$GIT_COMMIT?access_token=$GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"failure\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"http://ec2-54-171-39-20.eu-west-1.compute.amazonaws.com:8080/job/topology-builder-pipeline-verify/$BUILD_NUMBER/console\"}"
