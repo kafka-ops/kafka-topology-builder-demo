f382ad92b2c618ea0fe7867deab9304725c0e8ad
Pere Urbon <pere@confluent.io>
2020-05-03 08:46:42 +0200

    cleanup wrong merge commit

--- post-hook-failure.sh
+++ post-hook-failure.sh
@@ -2,8 +2,5 @@ curl "https://api.github.com/repos/purbon/kafka-topology-builder-demo/statuses/$
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"state\": \"failure\",\"context\": \"topology-builder-pipeline-verify\", \"description\": \"Jenkins\", \"target_url\": \"http://ec2-54-171-39-20.eu-west-1.compute.amazonaws.com:8080/job/topology-builder-pipeline-verify/$BUILD_NUMBER/console\"}"
-<<<<<<< HEAD
 
 exit 1
-=======
->>>>>>> 369f24390cdd0e0cd813506da664f2cb531373c9
