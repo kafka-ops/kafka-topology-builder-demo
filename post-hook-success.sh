05544148034925fdf0fa951a688a561df55d456e
Pere Urbón <purbon@users.noreply.github.com>
2020-11-05 11:30:30 +0100

    add try to origin field (#28)
    * add origin field
    
    * update post hooks

--- post-hook-success.sh
+++ post-hook-success.sh
@@ -1,4 +1,4 @@
-curl "https://api.github.com/repos/purbon/kafka-topology-builder-demo/statuses/$GITHUB_PR_HEAD_SHA?access_token=$GITHUB_TOKEN" \
+curl "https://api.github.com/repos/kafka-ops/kafka-topology-builder-demo/statuses/$GITHUB_PR_HEAD_SHA?access_token=$GITHUB_TOKEN" \
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"state\": \"success\",\"context\": \"topology-builder-pipeline-verify\", \"description\": \"Jenkins\", \"target_url\": \"http://ec2-54-171-39-20.eu-west-1.compute.amazonaws.com:8080/job/topology-builder-pipeline-verify/$BUILD_NUMBER/console\"}"
